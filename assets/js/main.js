76adf613f563ef534706558256b9410bbb31f4cf
Gabriela <gabriela_navarro@college.harvard.edu>
2023-09-30 19:47:23 +0500

    chages added

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,20 +6,6 @@ window.addEventListener('scroll', () => {
     nav.classList.remove('header_bg_black');
   }
 });
-
-//Swiper
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 5.5,
-//     spaceBetween: 5,
-//     loop: true,
-//     centerSlide: 'true',
-//     fade: 'true',
-//     gragCursor: 'true',
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//   });
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 6,
     spaceBetween: 5,
